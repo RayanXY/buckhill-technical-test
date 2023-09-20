8b116fdba1dd57d0fe96f20f5d537eac6770b935
Rayan Avelino <rayanodb@gmail.com>
2023-09-20 11:46:28 -0300

    fix: better seeding for testing

--- sql-queries/00-seeding-database.sql
+++ sql-queries/00-seeding-database.sql
@@ -23,6 +23,39 @@ VALUES
 	(1, 'product_uuid_1', 'Product X', 30.00, 'Cat Premium Food', '{"weight": "3 kg"}'),
 	(2, 'product_uuid_2', 'Product Y', 30.00, 'Cat Premium Vitamin', '{"weight": "0.5 kg"}');
 	
--- Relation Table
-INSERT INTO product_subcategory (product_id, subcategory_id) VALUES (1, 4), (2, 6);
-INSERT INTO product_subcategory (product_id, subcategory_id) VALUES (1, 7), (2, 11);
+-- Products Subcategories
+INSERT INTO product_subcategory (product_id, subcategory_id) VALUES (1, 4), (1, 7), (2, 6), (2, 11);
+
+-- Users
+INSERT INTO users (uuid, first_name, last_name, email, password, avatar, address, is_admin, is_marketing)
+VALUES 
+	('user_uuid_1', 'Rayan', 'Avelino', 'rayan@mail.com', 'user1', 'avatar_1', '123 St', 0, 0),
+	('user_uuid_2', 'Gabriella', 'Lima', 'gabi@mail.com', 'user2', 'avatar_2', '456 St', 0, 0);
+	
+-- Payments
+INSERT INTO payments (uuid, type, details)
+VALUES 
+  ('pay_uuid_1', 'Credit Card', '{"card_type": "Visa", "last_four": "1234"}'),
+  ('pay_uuid_2', 'PayPal', '{"email": "example@example.com"}');
+  
+-- Orders Statuses
+INSERT INTO orders_statuses (uuid, title) VALUES 
+  ('os_uuid_1', 'Processing'),
+  ('os_uuid_2', 'Shipped'); 
+ 
+-- Orders
+ INSERT INTO orders (user_id, payment_id, order_status_id, uuid, products, address, amount, delivery_fee, shipped_at) VALUES 
+  (1, 1, 1, 'order_uuid_1', '{"product_id": 1, "quantity": 2}', '{"street": "123 St.", "city": "Natal", "country": "Brazil"}', 2000.00, 20.00, NULL),
+  (2, 2, 2, 'order_uuid_2', '{"product_id": 2, "quantity": 1}', '{"street": "456 St.", "city": "Riacho de Santana", "country": "Brazil"}', 60.00, 10.00, NOW());
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
