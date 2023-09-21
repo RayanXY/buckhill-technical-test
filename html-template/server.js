b9bc48da2925d74e33c28910cea0dfeabbe4d244
Rayan Avelino <rayanodb@gmail.com>
2023-09-21 16:18:26 -0300

    feat: authenticates and retrieves a order from the API

--- html-template/server.js
+++ html-template/server.js
@@ -5,8 +5,13 @@ const { compile } = pkg;
 import { readFileSync } from 'fs';
 import axios from 'axios';
 
+// CONSTS
 const app = express();
 const port = 3000;
+
+const password = 'admin';
+const email = 'admin@buckhill.co.uk';
+const authUrl = 'http://pet-shop.buckhill.com.hr/api/v1/admin/login'
 
 // "public" folder
 app.use(express.static(path.resolve('./public')));
@@ -16,12 +21,15 @@ app.use('/public', express.static(path.resolve('./public')));
 app.use(express.static(path.resolve('./src')));
 app.use('/src', express.static(path.resolve('./src')));
 
+const orders = await getAllOrders()
+
 app.get('/', (req, res) => {
   const source = readFileSync('views/layouts/email-template.hbs', 'utf8');
   const template = compile(source);
 
   const data = {
     title: 'Email Template',
+    order: orders[0],
   };
 
   const html = template(data);
@@ -30,4 +38,52 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+});
+
+// FUNCTIONS
+async function authenticate() {
+  const requestBody = new URLSearchParams();
+
+  requestBody.append('email', email);
+  requestBody.append('password', password);
+
+  try {
+    const response = axios.post(authUrl, requestBody, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+    
+    return (await response).data.data.token
+  } catch(error) {
+    console.error('Authentication error:', error.response.data);
+    throw new Error('Authentication failed');
+  }
+}
+
+async function getAllOrders() {
+  const allOrdersUrl = 'http://pet-shop.buckhill.com.hr/api/v1/orders'
+  const token = await authenticate();
+
+  try {
+    const response = axios.get(allOrdersUrl, {
+      headers: {
+        'accept': '*/*',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+
+    return (await response).data.data
+  } catch(error) {
+    console.error('Error:', error);
+  }
+}
+
+// HELPERS
+pkg.registerHelper('upper', function(str) {
+  return str.toUpperCase();
+});
+
+pkg.registerHelper('date', function(date) {
+  return new Date(date).toLocaleDateString()
 });
